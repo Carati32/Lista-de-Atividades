a3f824ac9d8639185879a961e03920d24e4a7ca1
Carati32 <brunosilva33357@gmail.com>
2025-08-16 23:59:43 -0300

    Finalização das atividades.

--- script.js
+++ script.js
@@ -139,11 +139,13 @@ nomes.forEach(function (nome) {
 let numerosArray = [1,2,3,4,5]
 let numerosAoQuadrado = numerosArray.map(numero => numero * numero)
 console.log(numerosAoQuadrado)
+
 //12) filter
 //Crie um array de idades e filtre apenas as maiores ou iguais a 18.
 let idades = [5, 18, 1, 42, 9, 20]
 let maioresQueDez = idades.filter(numero => numero >= 18)
 console.log(maioresQueDez)
+
 //13) Função tradicional e arrow function
 // Crie uma função tradicional que receba dois números e retorne a soma, e depois faça o mesmo com arrow function.
 function tradicional(){
@@ -152,28 +154,59 @@ function tradicional(){
   let soma = numberone + numbertwo
   console.log("Function tradicional" +"\n" +"A soma dos números é " + soma)
 }
-
-console.log()
 //14)Função callback
 //Crie uma função que receba outra função como parâmetro e a execute (ex.: função que recebe um nome e imprime no console)
+function call(name, callback){
+    console.log(`Olá, ${name}`)
+    callback()
+}
 
+function boanoite(){
+  console.log("Boa noite")
+}
+call("Yuri", boanoite)
 
 //15) Função anônima
 // Crie uma função anônima atribuída a uma variável que receba um número e retorne se ele é par ou ímpar.
-
-
+let par = function(n){
+    if(n % 2 === 0){
+      return n + " é par"
+    }
+    else{
+      return n +" é ímpar"
+    }
+}
+console.log(par(100))
 
 //16) Manipulação de JSON
 // Crie um objeto com nome e idade, converta para JSON e exiba no console. Depois, converta de volta para objeto
-
+const obj = {nome:"Yuri", idade: 17}
+const json = JSON.stringify(obj)
+console.log(json)
+ 
+const jsonobj = JSON.parse(json)
+console.log(jsonobj)
 
 //17) Função assíncrona
 //Crie uma função async que aguarde 2 segundos e depois exiba "Carregou!".
+function aguardando(){
+  return new Promise(resolve =>{
+    setTimeout(() =>{
+      resolve("Carregou!")
+    }, 2000)
+  })
+}
 
-
+async function executar(){
+  console.log("Esperando carregar...")
+  const resultado = await aguardando()
+  console.log(resultado)
+}
+executar()
 //18)Definição de back-end
 // Explique, com suas palavras, o que é back-end e cite 3 exemplos de tecnologias usadas para desenvolvê-lo.
-
+//Back-end é a parte do site ou aplicativo que roda no servidor,responsável por processar dados,gerenciar banco de dados e lógica do sistema.
+//Exemplos: Node.js   Python   PHP.
 
 //19) Diferença entre server-side e client-side
 // Escreva uma tabela comparando server-side e client-side, incluindo:
@@ -181,27 +214,51 @@ console.log()
 //b) Exemplos de linguagens
 //c) Vantagens e desvantagens
 
+//Diferença entre Server-side e Client-side:
+
+//Aspecto: Onde o código é executado
+//- Server-Side: No servidor que hospeda o site.
+//- Client-Side: No navegador do usuário.
+
+//Aspecto: Exemplos de linguagens
+//- Server-Side: Node.js,PHP,Python,Ruby,Java.
+//- Client-Side: JavaScript HTML,CSS,TypeScript.
+
+//Aspecto: Vantagens
+//- Server-Side: Maior controle sobre dados,acesso a banco de dados,mais seguro.
+//- Client-Side: Resposta mais rápida,interatividade imediata,reduz carga no servidor.
+
+//Aspecto: Desvantagens
+//- Server-Side: Pode ser mais lento,maior carga no servidor.
+//- Client-Side: Menor segurança,depende do dispositivo do usuário.
+
 
 //20) Fluxo de funcionamento do back-end
 //Descreva um esquema simples mostrando o fluxo de dados quando um usuário acessa um site
-
+//Usuário > requisição > servidor > banco de dados > resposta > navegador > tela.
 
 //21) Primeiro script com Node.js
 // Crie um arquivo teste.js, digite qualquer coisa e execute no terminal chamando o comando do node
 
 
 //22) Verificação de instalação
-// Você baixou o Nodejs no seu computador, mas precisa verificar se foi instalado corretamente. Quais comandos você deve executar no terminal para confirmar a instalação?
-
+// Você baixou o Node.js no seu computador, mas precisa verificar se foi instalado corretamente. Quais comandos você deve executar no terminal para confirmar a instalação?
+//node -v
+//npm -v
 
 //23)NPM
 // O que é o npm e qual a sua função?
-
+//O npm é um gerenciador de pacotes para projetos em JavaScript,muito usado em aplicações node.js.Ele facilita a instalação,atualização,remoção e o compartilhamento de bibliotecas e módulos de código.
 
 //24) Exemplos reais de uso do back-end
 // Pesquise e liste 3 exemplos de funcionalidades de sistemas que dependem do back-end para funcionar.
-
+//1) Login e autenticação: Validar usuário/senha,criar sessões ou tokens
+//2) Processamento de regras de negócio: Exemplo:compras,pagamentos,cálculos de desconto.
+//3) Banco de dados: Salvar,buscar e atualizar informações(usuários,pedidos,produtos).
 
 //25)Protocolos HTTP
 // Liste 4 métodos HTTP e explique resumidamente para que serve cada um.
-
+//1) GET: Usado para buscar dados de um servidor(ex:carregar produtos de uma loja).
+//2) POST: Usado para enviar dados ao servidor e criar novos recursos(ex:cadastrar usuário).
+//3) PUT: Usado para atualizar um recurso existente por completo(ex:editar perfil de usuário).
+//4) DELETE: Usado para remover um recurso do servidor(ex:excluir conta).
---
+++ teste.js
@@ -0,0 +1,1 @@
+console.log("Frango é bom demais")
